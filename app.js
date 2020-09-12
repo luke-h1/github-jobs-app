ba28d2ec957b51d5791d4629cab8d75807153fbd
luke-h1 <luke.howsam@yahoo.com>
2020-09-12 15:42:58 +0100

    add vaalidation

--- app.js
+++ app.js
@@ -2,18 +2,6 @@ const form = document.getElementById('form-control');
 const jobPreference = document.getElementById('job-input');
 const div = document.getElementById('output');
 const loadingEl = document.getElementById('loading');
-async function fetchJobData(e) {
-  showLoading();
-  e.preventDefault();
-  const job = jobPreference.value;
-  // console.log(`Job: ${job}, area: ${area}`);
-  const API_URL = `https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=${job}&page=1
-  `;
-  const res = await fetch(`${API_URL}`);
-  const json = await res.json();
-  console.log(json);
-  showJobDataDOM(json);
-}
 
 function showLoading() {
   loadingEl.style.display = 'block';
@@ -21,6 +9,31 @@ function showLoading() {
 
 function hideLoading() {
   loadingEl.style.display = 'none';
+}
+
+function showError(message) {
+  const errorEl = document.getElementById('error');
+  errorEl.innerHTML = message;
+  setTimeout(() => {
+    errorEl.innerHTML = '';
+  }, 2000);
+}
+
+async function fetchJobData(e) {
+  e.preventDefault();
+  const job = jobPreference.value;
+  if (job === '') {
+    showError('Enter a correct search term');
+  } else {
+    showLoading();
+
+    const API_URL = `https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=${job}&page=1
+  `;
+    const res = await fetch(`${API_URL}`);
+    const json = await res.json();
+    console.log(json);
+    showJobDataDOM(json);
+  }
 }
 
 function showJobDataDOM(json) {
